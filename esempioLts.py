2822dae07d02c04503a219f919e444ec58e881f2
Stefano Martina <stefano.martina@gmail.com>
2016-01-11 12:41:50 +0100

    aggiunto esempi formule e abbozzi di metodi sat

--- esempioLts.py
+++ esempioLts.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-#provagithub
+
 import matplotlib.pyplot as plt
 import networkx as nx
+
+#LTS
 
 lts=nx.DiGraph()
 
@@ -27,12 +29,59 @@ lts.add_edge('s6', 's4')
 lts.add_edge('s7', 's3')
 lts.add_edge('s7', 's6')
 
-print('a (s1): {}'.format('a' in lts.node['s1']['att']))
-print('b (s1): {}'.format('b' in lts.node['s1']['att']))
-print('c (s1): {}'.format('c' in lts.node['s1']['att']))
-print('a=b (s1): {}'.format(('a' in lts.node['s1']['att']) == ('b' in lts.node['s1']['att'])))
+#print('a (s1): {}'.format('a' in lts.node['s1']['att']))
+#print('b (s1): {}'.format('b' in lts.node['s1']['att']))
+#print('c (s1): {}'.format('c' in lts.node['s1']['att']))
+#print('a=b (s1): {}'.format(('a' in lts.node['s1']['att']) == ('b' in lts.node['s1']['att'])))
 
 
-labels=dict((n,{'l':n,'d':d['att']}) for n,d in lts.nodes(data=True))
-nx.draw_networkx(lts, labels=labels)
+#labels=dict((n,{'l':n,'d':d['att']}) for n,d in lts.nodes(data=True))
+#nx.draw_networkx(lts, labels=labels)
+
+
+
+#Phi
+
+phi = nx.DiGraph()
+phi.add_node('f0', form='and')
+phi.add_node('f1', form='next')
+phi.add_node('f2', form='ap', val='a')
+phi.add_node('f3', form='until')
+phi.add_node('f4', form='ap', val='b')
+phi.add_node('f5', form='allways')
+phi.add_node('f6', form='not')
+phi.add_node('f7', form='ap', val='c')
+
+phi.add_edge('f0', 'f1')
+phi.add_edge('f0', 'f3')
+phi.add_edge('f1', 'f2')
+phi.add_edge('f3', 'f4')
+phi.add_edge('f3', 'f5')
+phi.add_edge('f5', 'f6')
+phi.add_edge('f6', 'f7')
+
+#labels=dict((n,{'l':n,'f':d['form'],'v':(d['val'] if 'val' in d.keys() else 'ND')}) for n,d in phi.nodes(data=True))
+#nx.draw_networkx(phi, labels=labels)
+
+
 #plt.show()
+
+phiTrue=nx.DiGraph()
+phiTrue.add_node('f0', form='true')
+
+def satTrue(nodo):
+    return set(lts.nodes())
+
+
+callDic = {
+    'true':satTrue,
+    }
+
+def sat(nodo):
+    if (phiTrue.node[nodo]['form'] in callDic.keys()) :
+        return callDic[phiTrue.node[nodo]['form']](nodo)
+
+
+print(sat('f0'))
+
+    
