5488589311cf43bccaeba812b5f2fe8499c58109
Stefano Martina <stefano.martina@gmail.com>
2016-01-23 19:29:07 +0100

    Implementato parser di formule CTL

--- ctlFormule.py
+++ ctlFormule.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import pyparsing as pp
 import syntax
 
 class CtlFormule(object):
@@ -7,33 +8,41 @@ class CtlFormule(object):
     Class that contains the tree for a CTL formula parsed from a
     configuration file passed during costruction.
     """
+
+    _syntax = syntax.Syntax()
+    _atom = pp.Word(pp.alphas)
+    _true = pp.Literal(_syntax.true)
+    _op = _atom | _true
+
+    _form = pp.operatorPrecedence( _op, [
+        (_syntax.lnot, 1, pp.opAssoc.RIGHT),
+        (pp.oneOf(_syntax.land+' '+_syntax.lor), 2, pp.opAssoc.LEFT),
+        (pp.oneOf(_syntax.implies+' '+_syntax.equals), 2, pp.opAssoc.LEFT),
+        (pp.oneOf(_syntax.exNext+' '+_syntax.exUntil+' '+_syntax.exAlways+' '+_syntax.exEventually+' '+_syntax.faNext+' '+_syntax.faUntil+' '+_syntax.faAlways+' '+_syntax.faEventually+' '+_syntax.exWeakUntil+' '+_syntax.faWeakUntil), 2, pp.opAssoc.LEFT)])
+
     
-    def __init__(self, filename):
-        self._syntax = syntax.Syntax()
+    def __init__(self, formulaString):
         self._graph = nx.DiGraph()
+        self._parseNode(self._form.parseString(formulaString)[0], isRoot=True)
         
-        f = open(filename, 'r')
-        nodesPart = True
-        firstLine = True
-        for line in f:
-            fields = line.split('//')[0].split()
-            if nodesPart and len(fields) == 0:
-                nodesPart = False
-                continue
-
-            if nodesPart:
-                if (fields[1] == self._syntax.ap):
-                    self._graph.add_node(fields[0], root=firstLine, form=fields[1], val=fields[2])
-                else:
-                    self._graph.add_node(fields[0], root=firstLine, form=fields[1])
-                firstLine = False
-
+    def _parseNode(self, nodeList, isRoot=False):
+        newId = nx.utils.misc.generate_unique_node()
+        if isinstance(nodeList, str):
+            if nodeList == self._syntax.true:
+                self._graph.add_node(newId, root=isRoot, form=self._syntax.true)
             else:
-                if len(fields) == 3:
-                    self._graph.add_edge(fields[0], fields[1], son=fields[2])
-                else:
-                    self._graph.add_edge(fields[0], fields[1])
+                self._graph.add_node(newId, root=isRoot, form=self._syntax.ap, val=nodeList)
+            
+        elif len(nodeList) == 2:
+            self._graph.add_node(newId, root=isRoot, form=nodeList[0])
+            self._graph.add_edge(newId, self._parseNode(nodeList[1]))
+        else: #len(nodeList) == 3
+            self._graph.add_node(newId, root=isRoot, form=nodeList[1])
+            self._graph.add_edge(newId, self._parseNode(nodeList[0]), son=self._syntax.leftSon)
+            self._graph.add_edge(newId, self._parseNode(nodeList[2]), son=self._syntax.rightSon)
 
+        return newId
+    
     def draw(self):
         labels=dict((n,{'l':n,'d':d['form']}) for n,d in self._graph.nodes(data=True))
         nx.draw_networkx(self._graph, labels=labels)
